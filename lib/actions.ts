a06967a461f068ef60dfef6c8596b629ed825b1f
moathssnck <myneww00@gmail.com>
2025-07-31 14:32:56 +0300

    Update actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,7 @@
 
 
 export const playNotificationSound = () => {
-    const audio=new Audio('/iphone.mp3')
+    const audio=new Audio('/update-notification.mp3')
     if (audio) {
       audio!.play().catch((error) => {
         console.error('Failed to play sound:', error);
