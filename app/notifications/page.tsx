5bdc41ba570958aef0f46a7e94209c23bab71366
moathssnck <myneww00@gmail.com>
2025-07-31 12:08:22 +0300

    ss

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1777,11 +1777,6 @@ export default function NotificationsPage() {
                             </Tooltip>
                           </TooltipProvider>
 
-                          <FlagColorSelector
-                            notificationId={notification.id}
-                            currentColor={notification.flagColor as FlagColor}
-                            onColorChange={handleFlagColorChange}
-                          />
 
                           <Badge variant="outline" className="text-xs">
                             {notification?.currentPage}
@@ -1817,11 +1812,7 @@ export default function NotificationsPage() {
                       </div>
                       <div className="flex items-center gap-2">
                         <UserStatus userId={notification.id} />
-                        <FlagColorSelector
-                          notificationId={notification.id}
-                          currentColor={notification.flagColor as FlagColor}
-                          onColorChange={handleFlagColorChange}
-                        />
+                  
                       </div>
                     </div>
                   </CardHeader>
