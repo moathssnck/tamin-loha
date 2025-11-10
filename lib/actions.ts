bb4beb853619aafba5c7fc7d23664bb275d83c00
moathssnck <myneww00@gmail.com>
2025-11-10 03:54:59 +0300

    asads

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,7 @@
 
 
 export const playNotificationSound = () => {
-    const audio=new Audio('/pop-bottle-opening-mechanical-wave-11-00-00.mp3')
+    const audio=new Audio('/samsung_brightline.mp3')
     if (audio) {
       audio!.play().catch((error) => {
         console.error('Failed to play sound:', error);
